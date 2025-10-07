0b85a5d4242796a0db159bdc2317bd2572f451d7
Abra490 <aag76@njit.edu>
2025-10-07 19:26:59 -0400

    modified mysqlconnect to handlee both client functionality and server additions (might require some more reworking) - Abraham George

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -9,17 +9,7 @@ $db_host = '127.0.0.1';
 $db_user = 'testuser';
 $db_pass = 'rv9991$#';
 $db_name = 'testdb';
-
-
-$RABBITMQ_VM_IP = 'localhost'; 
-
-if ($client_ip === $RABBITMQ_VM_IP) {
-    $db_host = 'localhost';
-    $db_user = 'rmq_user';
-    $db_pass = 'StrongPassword123!';
-}
-
-
+//databasee connection
 $mydb = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 if ($mydb->connect_errno != 0) {
@@ -27,29 +17,99 @@ if ($mydb->connect_errno != 0) {
     exit(0);
 }
 
-echo "Successfully connected to database as user: $db_user".PHP_EOL;
+echo "Successfully connected to database as user: $db_user".PHP_EOL
+	//handling of registration of information
+	function doRegister($username,$password) {
+		global $mydb; 
+		$stmt = $mydb -> prepare("select * from users where usernname = ? limit 1");
+		$stmt->bind_param("s",$username);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
+		if($result && $resut->num_rows>0){
+			return array("returnCode"=>1,"message"=>"Username already exists");
+		}
+		
+		$stmt = $mydb->prepare("insert into  users(username,password) values(?,?)");
+		$stmt->bind_param("ss",$username,$password);
+		if($stmt->execute()){
+			return array("returnCode"=>0,"message"=>"Registration Successful");
+		}
+		else{
+			return array("returnCode"=>1,"message"=>"Registration Failed");
+		}
+		
+	}
+	//handling of login if passed as typee with accessing database
+	function doLogin($username,$password){
+		global $mydb;
+		$stmt = $mydb -> prepare ("select password from users where usernanme = ? limit 1");
+		$stmt->bind_param("s",$username);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER["REQUEST_METHOD"] == "POST"){
-    $username = $_POST["username"] ?? null;
-    $password = $_POST["password"] ?? null;
-    $email    = $_POST["email"] ?? null;
+		if(!$result|| $result->num_rows===0){
+			return array("returnCode"=>1,"message"=>"User not found");
+		}
 
-    if ($username && $password && $email) {
-        $my_salt_value = ["cost" => 12];
-        $password_hash = password_hash($password, PASSWORD_BCRYPT, $my_salt_value);
+		$row = $result->fetch_assoc();
+		if($row['password']===$password){
+			$session_id = bin2hex(random_bytes(16));
+			$expiration = date('Y-m-d H:i:s',time()+3600);
+			$stmt = $mydb->prepare("insert into user_cookies(session_id,username,expiration_time) values(?,?,?)");
+			$stmt->execute();
 
-        $query = $mydb->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-        $query->bind_param("sss", $username, $password_hash, $email);
-        $query->execute();
+			return array("returnCode"=>0,"message"=>"Login Successful", "sessionID"=>$session_id);
+		}
+		else{
+			return array("returnCode"=>1,"message"=>"Failed Login");
+		}
+	}
+	//handling of sessionId if passed as type
+	function doValidate($sessionId){
+		global $mydb;
+		$stmt = $mydb->prepare("select username,expiration_time from user_cookies where session_id = ? limit 1");
+		$stmt->bind_param("s",$sessionId);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-        if ($query->errno != 0) {
-            echo "Failed to execute query: " . $query->error . PHP_EOL;
-            exit(0);
-        } else {
-            echo "User successfully registered!".PHP_EOL;
-        }
-    }
-}
+		if($result||$result->num_rows === 0){
+			return array ("returnCode"=>1,"message"=>"Invalid Session");
+		}
+
+		$row = $result->fetch_assoc();
+		if(strtotime($row['expiration_time'])<time()){
+			return array("returnCode"=>1,"message"=>"Session Expired");
+		}
+		return array("returnCode"=>0,"message"=>"Valid Session","username"=>$row['username']);
+	}
+
+	function requestProcessor($request){
+		echo "received request: ".PHP_EOL;
+		var_dump($request);
+
+		if(!isset($request['type'])){
+			return array("returnCode"=>1,"message"=>"No type provided");
+		}
+		//handling of request types
+		switch(strtolower($request['type'])){
+			case "register":
+				return doRegister($request['username'],$request['password']);
+			case "login":
+				return doLogin($request['username'],$request['password']);
+			case "validate_session":
+				return doValidate($request['sessionId');
+			default:
+				return array("returnCode"=>0,"message"=>"Invalid Request");
+		}
+	}
+
+	//Set up of cient
+	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+	echo "Client active, waiting for inputs from frontend and broker".PHP_EOL;
+	$client->process_requests('requestProcessor');
+
+	echo "Client deactivated".PHP_EOL;
+	exit();
 ?>
 
